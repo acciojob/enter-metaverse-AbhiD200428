81c2ff3ead8c2ddc4e4b143e76bd291b40340066
Abhilash Dash <abhilash200428@gmail.com>
2026-03-16 21:58:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//your JS code here. If required.
 const textBox = document.getElementById("status");
-textBox.textContent = "Enter the Metaverse";
+
+textBox.innerHTML = "<h1>Enter the Metaverse</h1>";
+
 function versePortal() {
-	textBox.style.fontSize = "30px";
-	textBox.style.weight = "bold";
-	textBox.textContent = "Entered Metaverse";
+    textBox.style.fontSize = "30px";
+    textBox.style.fontWeight = "bold";
+    textBox.innerHTML = "<h1>Entered Metaverse</h1>";
 }
