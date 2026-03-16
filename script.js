1a61797ad9e6fd30fa4084b03553087d0961cd8a
Abhilash Dash <abhilash200428@gmail.com>
2026-03-16 21:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const textBox = document.getElementById("status");
+textBox.textContent = "Enter the Metaverse";
+function versePortal() {
+	textBox.style.fontSize = "30px";
+	textBox.style.weight = "bold";
+	textBox.textContent = "Entered Metaverse";
+}
